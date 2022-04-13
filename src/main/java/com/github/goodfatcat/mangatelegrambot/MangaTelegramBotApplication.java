61493326fdfc37c1b92e41f66813695e7179608e
GoodFatCat <osertupokrushki92@gmail.com>
2022-04-13 19:23:02 +0700

    Added springboot and maven project

---
+++ src/main/java/com/github/goodfatcat/mangatelegrambot/MangaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.goodfatcat.mangatelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/goodfatcat/mangatelegrambot/MangaTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.goodfatcat.mangatelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
